a6fd4d05883fb72b638221a301d8d6f6aa4c4780
baitianl <baitianl@gmail.com>
2024-03-28 21:20:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,5 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/kenzok8/openwrt-packages/luci-app-easymesh package/luci-app-easymesh
